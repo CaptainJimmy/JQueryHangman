2bed964107224b99adcab23f218da33290b1e671
CaptainJimmy <captainjimscharters@gmail.com>
2017-09-06 18:24:16 -0400

    clues and functionality added

--- assets/js/hangman.js
+++ assets/js/hangman.js
@@ -3,8 +3,8 @@ $(document).ready(function(){
 
 
 //variables
-var wins;
-var losses;
+var wins=0;
+var losses=0;
 var lettersGuessed=[];
 var wordBank=[];
 var guessesLeft;
@@ -25,25 +25,42 @@ var keyboard={
 
 function RandomWord() {
 	console.log("RandomWord");
-        var requestStr = "http://api.wordnik.com:80/v4/words.json/randomWords?hasDictionaryDef=true&minCorpusCount=0&minLength=5&maxLength=15&limit=1&api_key=a2a73e7b926c924fad7001ca3111acd55af2ffabf50eb4ae5";
+        var requestStr = "http://api.wordnik.com:80/v4/words.json/randomWords?hasDictionaryDef=true&includePartOfSpeech=noun&excludePartOfSpeech=conjunction&minCorpusCount=0&minLength=5&maxLength=15&limit=1&api_key=a2a73e7b926c924fad7001ca3111acd55af2ffabf50eb4ae5";
 
         $.ajax({
             type: "GET",
             url: requestStr
         }).done(function(result) {
-        	//console.log(result);
        pickedWord = result[0].word;
-        console.log(pickedWord);
-        return pickedWord;
+   		pickedWordArray=pickedWord.split("");
+   		console.log(pickedWordArray);
+   		displayWord();
     })
  }
 
+ function displayWord(){
+ 	console.log("displayword");
+ 	$('#picked-word-array').empty();
+ 	$('#picked-word-array').html($('<h2>').text("Your Clue: " ));
+ 	var pickedWordSendIt="";
+for (var i=0; i < pickedWordArray.length; i++){
+		pickedWordSendIt=($('<button>').addClass("btn btn-danger").attr("index-value",i)).append($('<i>').addClass("fa fa-minus fa-2x"));
+		//pickedWordSendIt.append($('<span>').addClass("glyphicons glyphicons-minus"));
+		$('#picked-word-array').append(pickedWordSendIt);
+		}
+		
+			//.append(pickedWordSendIt));
+ 		$('#guesses-left').html("Guesses Left: " + guessesLeft);
+ 		$('#guesses-made').html("Guesses Made So Far: ");
+ }
+
+
 function newGame(){
-	// grab a new word
-	RandomWord();
-	console.log(pickedWord);
 	// reset guesses left
-	guessesLeft=0;
+	guessesLeft=10;
+	//display the score
+	 $('#wins').text("Wins: " + wins);
+ 	$('#losses').text("Losses: " + losses);
 	// empty out pickedWordPlaceholders
 	$('#picked-word-array').empty();
 	// empty out lettersGuessed
@@ -57,14 +74,14 @@ function newGame(){
 //create the first row
 	for (var i=0; i < keyboard.row1.length ; i++ ){
 		var newButton=$('<button>');
-		newButton.attr("class","btn btn-danger keyboardButton").text(keyboard.row1[i]).attr("data-value",keyboard.row1[i]);
+		newButton.attr("class","btn btn-primary keyboardButton").text(keyboard.row1[i]).attr("data-value",keyboard.row1[i]);
 		$('#keyboard-id').append(newButton);
-	}
+		}
 	//create the second row
 	$('#keyboard-id').append($('<div>').addClass("row2"));
 	for (var i=0;i < keyboard.row2.length;i++){
 		var newButton=$('<button>');
-		newButton.attr("class","btn btn-warning keyboardButton").text(keyboard.row2[i]).attr("data-value",keyboard.row2[i]);
+		newButton.attr("class","btn btn-danger keyboardButton").text(keyboard.row2[i]).attr("data-value",keyboard.row2[i]);
 		$('#keyboard-id').append(newButton);
 		}
 		//create the third row
@@ -74,13 +91,40 @@ function newGame(){
 		newButton.attr("class","btn btn-info keyboardButton").text(keyboard.row3[i]).attr("data-value",keyboard.row3[i]);
 		$('#keyboard-id').append(newButton);
 		}
+	// grab a new word
+	RandomWord();
+}
+	
+	
 
 
 
-	}
 
-	// split pickedWord into an array (array.split(""))
+ $(document).on('click','#new-game', function(event){
+ 	event.preventDefault();
+ 	newGame();
+});
+
+});
+
+
+
+//event listeners
+
+ //$('#new-game').on('keyup', function(event){
+// console.log(event);
+// var keyVal = event.whatever
+
+// if keyVal is already in the lettersGuessedArray
+	// if NOT push keyval into lettersguessedarray
+	//letterPress(keyVal)
+
+//})
+
+
+// split pickedWord into an array (array.split(""))
 	// loop over pickedWordArray and create blank dashes for our placeholders
+
 
 	// placeholders.join(" ") to the DOM
 
@@ -100,50 +144,3 @@ function newGame(){
 //}
 
 
-//event listeners
-
- //$('#new-game').on('keyup', function(event){
-// console.log(event);
-// var keyVal = event.whatever
-
-// if keyVal is already in the lettersGuessedArray
-	// if NOT push keyval into lettersguessedarray
-	//letterPress(keyVal)
-
-//})
-
-
- $(document).on('click',$('#new-game'), function(event){
- 	event.preventDefault();
- 	newGame();
-});
-
-});
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-//brackets for document ready, remove comment later
-//});
