9252fc7803816e60264d931d7883e0c6b555ef39
Jim Reinknecht <Jim Reinknecht>
2017-09-07 18:33:21 -0400

    fixed API to use SSL

--- assets/js/hangman.js
+++ assets/js/hangman.js
@@ -27,7 +27,7 @@ var keyboard={
 
 function RandomWord() {
 	console.log("RandomWord");
-        var requestStr = "http://api.wordnik.com:80/v4/words.json/randomWords?hasDictionaryDef=true&includePartOfSpeech=noun&excludePartOfSpeech=conjunction&minCorpusCount=0&minLength=5&maxLength=15&limit=1&api_key=a2a73e7b926c924fad7001ca3111acd55af2ffabf50eb4ae5";
+        var requestStr = "https://api.wordnik.com/v4/words.json/randomWords?hasDictionaryDef=true&includePartOfSpeech=noun&excludePartOfSpeech=conjunction&minCorpusCount=0&minLength=5&maxLength=15&limit=1&api_key=a2a73e7b926c924fad7001ca3111acd55af2ffabf50eb4ae5";
 
         $.ajax({
             type: "GET",
